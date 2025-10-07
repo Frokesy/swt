a6ffb2730b5575b134346c4c0e3509b724e3906f
Ayanfeoluwa Noah Akindele <akindeleayanfeoluwa@gmail.com>
2025-10-07 06:28:33 +0100

    project reinitialised with TS, react and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { Canvas } from "@react-three/fiber"
-import { Suspense } from "react"
-import Three from "./components/Three"
-import "./App.css"
-function App() {
-  // bg-[#f1f1f1]
-  return (
-    <Canvas className="bg-[#000]" id="canvas" shadows>
-      <Suspense fallback={null}>
-        <Three />
-      </Suspense>
-    </Canvas>
-  )
-}
+const App = () => {
+  return <div className="text-red-500">Hello, World!</div>;
+};
 
-export default App
+export default App;
